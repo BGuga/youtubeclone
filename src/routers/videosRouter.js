183919fc2507cd7d91b46ea5f4581ed732b19173
SeungJun Baek <tmdwns9912@naver.com>
2022-05-23 23:56:22 +0900

    라우터까지 세팅 완료

---
+++ src/routers/videosRouter.js
@@ -0,0 +1,8 @@
+import express from "express";
+
+const videoRouter = express.Router();
+
+videoRouter.get("/watch", handleWatchVideo);
+
+const handleWatchVideo = (req, res) =>res.send("Watch Video");
+
