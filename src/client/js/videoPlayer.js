084483a0585685095a19fd6770e1c86859193a0a
Guga <103228463+BGuga@users.noreply.github.com>
2022-08-01 04:52:35 +0900

    video duration error edit #3

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -130,7 +130,7 @@ playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMuteClick);
 volumeRange.addEventListener("input", handleVolumeChange);
 video.addEventListener("canplay", handleLoadedMetadata);
-handleLoadedData();
+handleLoadedMetadata();
 video.addEventListener("timeupdate", handleTimeUpdate);
 video.addEventListener("ended",handleEnded);
 timeline.addEventListener("input", handleTimelineChange);
