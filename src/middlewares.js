de52da2862b92df6cd6749b361b30728a9a7fd5d
Guga <103228463+BGuga@users.noreply.github.com>
2022-08-06 16:48:34 +0900

    url buf fix

--- src/middlewares.js
+++ src/middlewares.js
@@ -9,7 +9,7 @@ const s3 = new aws.S3({
     }
 })
 
-const isHeroku = process.env.NODE_ENV ==="production";
+const isHeroku = process.env.Herokuconfirm ==="true";
 
 const s3ImageUploader = multerS3({
     s3: s3,
@@ -63,5 +63,5 @@ export const videoUpload = multer({
     limits:{
     fileSize:100000000,
     },
-    storage : s3VideoUploader,
+    storage : isHeroku ? s3VideoUploader : undefined,
 });
