1fecd87b978626dd55dbd3807bbdb68c34c8d8ae
Guga <103228463+BGuga@users.noreply.github.com>
2022-09-03 02:16:51 +0900

    what happen if aws.s3 don't have credentials>

--- src/middlewares.js
+++ src/middlewares.js
@@ -2,68 +2,60 @@ import multer from "multer";
 import multerS3 from "multer-s3";
 import aws from "aws-sdk";
 
-const s3 = new aws.S3({
-    credentials:{
-        accessKeyId:process.env.AWS_ID,
-        secretAccessKey:process.env.AWS_SECRET
-    }
-})
+const s3 = new aws.S3();
 
-const isHeroku = process.env.Herokuconfirm ==="true";
+const isHeroku = process.env.Herokuconfirm === "true";
 
 const s3ImageUploader = multerS3({
-    s3: s3,
-    bucket: 'wetubeguga/images',
-    acl: 'public-read',
-    contentType: multerS3.AUTO_CONTENT_TYPE,
-})
+  s3: s3,
+  bucket: "wetubeguga/images",
+  acl: "public-read",
+  contentType: multerS3.AUTO_CONTENT_TYPE,
+});
 
 const s3VideoUploader = multerS3({
-    s3: s3,
-    bucket: 'wetubeguga/videos',
-    acl: 'public-read',
-    contentType: multerS3.AUTO_CONTENT_TYPE,
-}) 
+  s3: s3,
+  bucket: "wetubeguga/videos",
+  acl: "public-read",
+  contentType: multerS3.AUTO_CONTENT_TYPE,
+});
 
-export const localsMiddleware = (req, res, next)=>{
-    res.locals.loggedIn = Boolean(req.session.loggedIn);
-    res.locals.siteName = "Wetube";
-    res.locals.loggedInUser = req.session.user || {};
-    next();
+export const localsMiddleware = (req, res, next) => {
+  res.locals.loggedIn = Boolean(req.session.loggedIn);
+  res.locals.siteName = "Wetube";
+  res.locals.loggedInUser = req.session.user || {};
+  next();
 };
 
-
-
-export const protectorMiddleware = (req, res, next)=>{
-    if(req.session.loggedIn){
-        return next()
-    } else {
-        req.flash("error", "Log in first");
-        return res.redirect("/login");
-    }
+export const protectorMiddleware = (req, res, next) => {
+  if (req.session.loggedIn) {
+    return next();
+  } else {
+    req.flash("error", "Log in first");
+    return res.redirect("/login");
+  }
 };
 
+export const publicOnlyMiddleware = (req, res, next) => {
+  if (!req.session.loggedIn) {
+    return next();
+  } else {
+    req.flash("error", "Not authorized");
+    return res.redirect("/");
+  }
+};
 
-export const publicOnlyMiddleware = (req, res, next)=>{
-    if(!req.session.loggedIn){
-        return next();
-    } else{
-        req.flash("error", "Not authorized");
-        return res.redirect("/")
-    }
-}
-
-export const avatarUpload= multer({
-    dest:"uploads/avatars/", 
-    limits:{
-        fileSize:3000000,
-    },
-    storage : isHeroku ? s3ImageUploader : undefined,
+export const avatarUpload = multer({
+  dest: "uploads/avatars/",
+  limits: {
+    fileSize: 3000000,
+  },
+  storage: isHeroku ? s3ImageUploader : undefined,
 });
 export const videoUpload = multer({
-    dest:"uploads/videos/", 
-    limits:{
-    fileSize:100000000,
-    },
-    storage : isHeroku ? s3VideoUploader : undefined,
+  dest: "uploads/videos/",
+  limits: {
+    fileSize: 100000000,
+  },
+  storage: isHeroku ? s3VideoUploader : undefined,
 });
